52252b8d8d0917a71dde3fdf0296d7f8e79002c8
dimas pradipto <dpradipto8@gmail.com>
2026-08-04 06:17:53 +0700

    feat: add vendor libraries for dashboard UI and integrate with base templates

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(AuthController::class)->group(function(){
+
+    Route::get('/', 'login')->name('login');
+    Route::post('/', 'authenticate')->name('login.post');
+    Route::post('/logout', 'logout')->name('logout');
 });
